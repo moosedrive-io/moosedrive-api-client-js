4d8e7dd5394d7de6ffadc0fe67f5c7e7affb3f2e
Anders Pitman <tapitman11@gmail.com>
2019-06-12 22:46:09 -0600

    Use omni-rpc

--- src/index.js
+++ src/index.js
@@ -1,40 +1,14 @@
 import { encodeObject, decodeObject, initiateWebSocketMux } from 'omnistreams';
 import { FileReadProducer } from 'omnistreams-filereader';
+import { Peer as RPCPeer } from 'omni-rpc';
 
 
 class Client {
 
-  constructor(mux) {
-
-    this._nextRequestId = 1;
-    this._mux = mux;
+  constructor(rpc) {
     this._authKey = null;
-    this._requests = {};
-    
-    mux.onControlMessage((rawMessage) => {
-      const message = decodeObject(rawMessage)
-      console.log(message);
 
-      if (message.response !== undefined) {
-        console.log("response for: ", message.id);
-      }
-
-      delete this._requests[message.id];
-    });
-
-    mux.onConduit((producer, rawMeta) => {
-      const meta = decodeObject(rawMeta)
-      console.log(meta);
-
-      if (meta.response === true) {
-        this._requests[meta.id].resolve(producer);
-      }
-      else {
-        this._requests[meta.id].reject(meta.response);
-      }
-
-      delete this._requests[meta.id];
-    });
+    this._rpc = rpc;
   }
 
   setAuthKey(key) {
@@ -45,68 +19,37 @@ class Client {
 
     const fileStream = new FileReadProducer(file);
 
-    const consumer = this._mux.createConduit(encodeObject({
-      id: this._nextRequestId++,
-      jsonrpc: '2.0',
-      method: 'uploadFile',
-      params: {
-        key: this._authKey,
-        path,
-      },
-    }));
-
-    fileStream.pipe(consumer);
-    fileStream.onTermination(() => {
-      console.error("terminated");
-    })
+    this._rpc.requestSendStream('uploadFile', {
+      key: this._authKey,
+      path,
+      file,
+    }, fileStream);
   }
 
   saveTextFile(path, text) {
-    this._mux.sendControlMessage(encodeObject({
-      id: this._nextRequestId++,
-      jsonrpc: '2.0',
-      method: 'saveTextFile',
-      params: {
-        key: this._authKey,
-        path,
-        text,
-      },
-    }));
+    this._rpc.request('saveTextFile', {
+      key: this._authKey,
+      path,
+      text,
+    });
   }
 
   initBackup(srcPath, dstPath) {
-    this._mux.sendControlMessage(encodeObject({
-      id: this._nextRequestId++,
-      jsonrpc: '2.0',
-      method: 'initBackup',
-      params: {
-        key: this._authKey,
-        srcPath,
-        dstPath
-      },
-    }));
+    this._rpc.request('initBackup', {
+      key: this._authKey,
+      srcPath,
+      dstPath
+    });
   }
 
   async download(path, range) {
 
-    const requestId = this._nextRequestId++;
-
-    this._mux.sendControlMessage(encodeObject({
-      id: requestId,
-      jsonrpc: '2.0',
-      method: 'download',
-      params: {
-        key: this._authKey,
-        path,
-        range,
-      },
-    }));
-
-    return new Promise((resolve, reject) => {
-      this._requests[requestId] = { resolve, reject };
+    return await this._rpc.requestReceiveStream('download', {
+      key: this._authKey,
+      path,
+      range,
     });
   }
-
 }
 
 class ClientBuilder {
@@ -146,7 +89,9 @@ class ClientBuilder {
       secure: this._secure,
     });
 
-    const client = new Client(mux);
+    const rpc = new RPCPeer(mux);
+
+    const client = new Client(rpc);
 
     client.setAuthKey(this._authKey);
 
